9f2ff258624f900ffe2d9f0893394f059e595f83
smile6023326 <48814064+smile6023326@users.noreply.github.com>
2019-05-30 12:31:26 +0800

    Add files via upload

---
+++ 17.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	
+	int i,j;
+	int temp;
+	int input_num[5];
+	printf("找最小值\n");
+	printf("輸入5個數值:");
+	scanf(" %d %d %d %d %d", &input_num[0],&input_num[1],&input_num[2],&input_num[3],&input_num[4]);
+	
+	for(i=0;i<5;i++){
+		for(j=i+1;j<5;j++){
+			if(input_num[j]<input_num[i]){
+				temp=input_num[j];
+				input_num[j]=input_num[i];
+				input_num[i]=temp;
+			}
+		}
+	}
+	printf("\n\n");
+	printf("最小值為:%d\n",input_num[0]);
+	system("pause");
+	return 0;
+	
+}
